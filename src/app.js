6b473f820120abe128cbed5739f30a1a86734bb4
denis9diaz <150247992+denis9diaz@users.noreply.github.com>
2024-01-22 19:52:52 +0000

    cambios denis

--- src/app.js
+++ src/app.js
@@ -1,22 +1,19 @@
-window.onload = () => {
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
+  window.onload = () => {
+    const who = ["The dog", "My grandma", "His turtle", "My bird"];
+    const action = ["ate", "peed", "crushed", "broke"];
+    const what = ["my homework", "the keys", "the car"];
+    const when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+  
+    let message = `${selectItemOfArray(who)} ${selectItemOfArray(action)} ${selectItemOfArray(what)} ${selectItemOfArray(when)}`;
+    document.getElementById("excuse").textContent = message;
 
-  function selectItemOfArray(array) {
-    return array[Math.floor(Math.random() * array.length)];
-  }
 
-  let message = `${selectItemOfArray(who)} ${selectItemOfArray(action)}
-   ${selectItemOfArray(what)} ${selectItemOfArray(when)}`;
-  document.getElementById("excuse").textContent = message;
 
   // Imprimir en la consola los elementos seleccionados
   /*var who1 = document.querySelector("#mensaje1").innerHTML;
